14677b8b0c75f54932d18b3e78a5500fbac1df73
Youssef sameh <yousameh2006@gmail.com>
2022-11-10 04:16:30 +0200

    new commit

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express')
-const app = express()
-require('dotenv').config()
-const passport = require('passport')
-const mongoose = require('mongoose')
+   const app = express()
+     require('dotenv').config()
+       const passport = require('passport')
+         const mongoose = require('mongoose')
+           const bcrypt = require('bcrypt')
 
+ 
+        app.listen(9000, () => {
+             console.log('server is running on port 9000 ...');
+            });
 
+        mongoose.connect('', // your database connection string 
+          { useNewUrlParser: true, useUnifiedTopology: true}, () => {
+                 console.log("Database Connected ...");
+            });
 
-app.listen(9000, () => {
- console.log('server is running on port 9000 ...');
-});
+           const userSchema = new mongoose.Schema({
+            username: { type: String, required: true },
+              password: { type: String, required: true },  
+               }, { versionKey: false });
+           
+                 const User = mongoose.model("User", userSchema);
+           
+              // create a new user (Register)   
+            app.post('/', async (req, res) => {
+             const hashedPass = await bcrypt.hash(req.body.password, 10);
+              const newUser = new User({ username: req.body.username, password: hashedPass });
+               newUser.save().then(result => {
+                 res.json(result)
+                   }).catch(error => {
+                     res.json({ msg: error.message }); 
+                   });
+                 });    
