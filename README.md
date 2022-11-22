0d11b7dd465ff561fbdec78c076090ea233310d1
Youssef sameh <yousameh2006@gmail.com>
2022-11-22 03:53:06 +0200

    committed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# JWT
+#                                      JWT  
+            
+        1 - What is the different between ( Cookies, Sessions, Local-Storage )  ??
+    
+        Cookies: 
+            
+        Sessions:
+
+        Local-Storage:  
+
+
--- app.js
+++ app.js
@@ -2,17 +2,14 @@
 const express = require('express')
    const app = express()
      require('dotenv').config()
-       const passport = require('passport')
          const mongoose = require('mongoose')
            const bcrypt = require('bcrypt')
-             const LocalStrategy = require("passport-local").Strategy;
-
+            const jwt = require('jsonwebtoken')
+              
 
            // Middlewares
         app.use(express.urlencoded({ extended: true }))
         app.use(express.json())
-        app.use(passport.initialize());
-
         
 
          
@@ -29,40 +26,71 @@ const express = require('express')
            // create a user model for Registration process
            const userSchema = new mongoose.Schema({
             username: { type: String, required: true },
-              password: { type: String, required: true },  
+            email: { type: String, required: true },
+            password: { type: String, required: true },  
                }, { versionKey: false });
            
                  const User = mongoose.model("User", userSchema);
            
             
-                  // Home Page
-            app.get('/', (req, res) => {
-                res.send('Welcome !!');
-            });     
+  
 
 
 
                  // create a new user (Registration)   
-            app.post('/api/register', async (req, res) => {
+            app.post('/register', async (req, res) => {
+              const { username, email, password } = req.body;
              const hashedPass = await bcrypt.hash(req.body.password, 10);
-               const newUser = new User({ username: req.body.username, password: hashedPass });
-                 newUser.save().then(result => {
-                   res.json({
-                    username: result.username,
-                     password: result.password
-                      })
-                       }).catch(error => {
-                        res.json({ msg: error.message }); 
-                      });
-                   });    
-
-
+               const newUser = new User(req.body, hashedPass);
+                 newUser.save()
+                 res.json(newUser)
+              });    
                 
 
 
-                // login by this user 
-                app.post('/api/login', /*passport middleware*/ (req, res) => {
 
-                });
+            
+            app.post('/login', (req, res) => {
+              const id = new Date().getDate();
+               const { username, password } = req.body;
+                const user = { id, username };
+                 const userData = User.findOne({ username: username })
+                  if (!userData) { res.json({ msg: 'Invalid Credentials' }) };
+                
+                    const matchingPass =  bcrypt.compare(password, userData.password);
+                     if (!matchingPass) { res.json({ msg: "Incorrect Password" }) }
 
-              
+                      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '25d' });
+                                      
+                         res.status(200).json({ token });
+                     });
+                 
+                  
+                  
+
+           const authenticationMiddleware = async (req, res, next) => {
+            // you can access token after saving it in headers by the next line 
+             const authHeader = req.headers.token
+           
+             if (!authHeader || !authHeader.startsWith('Bearer ')) {
+                 res.json({ msg: "No Token Provided" });
+             }
+           
+             const accessToken = authHeader.split(' ')[1]
+             try {
+               const decoded = jwt.verify(accessToken, process.env.JWT_SECRET)
+               const { id, username } = decoded
+               req.user = { id, username }
+               next()
+             } catch (error) {
+               res.json({ msg: "Sorry, You can't Access This Route" });
+             }
+           } 
+                  
+
+
+
+
+            app.get('/data',  authenticationMiddleware, (req, res) => {
+               res.json({ msg: "Token Provided, Authorized successfully ..." });
+           });     
