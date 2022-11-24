4ac295020a9b1d887bac346f8b408180d4b3158e
Y o u s s e f - E l s i s y <110091301+usefsame7@users.noreply.github.com>
2022-11-24 03:54:55 +0200

    <>

--- app.js
+++ app.js
@@ -12,32 +12,32 @@ const app = express()
   app.use(express.json())
   
    
-     // connect to server
+         // connect to server
       app.listen(7000, () => {
          console.log('server is running on port 7000 ...');
       });
         
-     // connect ot database
+         // connect ot database
       mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true ,useUnifiedTopology: true })
       .then(() => console.log("Database connected ..."))
       .catch(err => console.log( err.message ));
-
-     // create a user model for Registration process
+ 
+         // create a user model for Registration process
      const userSchema = new mongoose.Schema({
       username: { type: String, required: true },
       password: { type: String, required: true },  
       }, { versionKey: false });
+
      
-           const User = mongoose.model("User", userSchema);
+        const User = mongoose.model("User", userSchema);
      
-      
 
            // create a new user (Registration)   
       app.post('/register', async (req, res) => {
         const { username, password } = req.body;
          const hashedPass = await bcrypt.hash(password, 10);
           const newUser = new User(req.body, hashedPass);
-          // Check If Passed Usernam Is Used Before Or Not
+                 // Check If Passed Usernam Is Used Before Or Not
              User.findOne({ username: username }).then(result => {
               if (result) {
                  res.json({ msg: "Username Used Before User Another One" });
